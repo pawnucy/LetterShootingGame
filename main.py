d866a3037b106d6d2bd8ee5dcb3f4056e42a4c9d
Paweł <pawelpuchalski0@gmail.com>
2023-03-20 11:56:50 +0100

    The level system has been added.

--- main.py
+++ main.py
@@ -6,36 +6,53 @@ from letters import Alphabet
 
 
 class Score:
+    """ Class responsible for score. """
     def __init__(self, settings, screen):
         self.score = 0
         self.settings = settings
         self.screen = screen
+        self.level = 1
 
-    def update(self):
+    def draw(self):
+        # Draw score on screen.
         score_font = self.settings.game_font
         score_text = score_font.render(f"Score: {self.score}", True, (255, 255, 255))
         score_rect = score_text.get_rect(topright=(self.settings.screen_width - 100, 10))
         self.screen.blit(score_text, score_rect)
+        # Draw level on screen.
+        level_font = self.settings.game_font
+        level_text = level_font.render(f"Level: {self.level}", True, (255, 255, 255))
+        level_rect = level_text.get_rect(midtop=(self.settings.screen_width / 2, 10))
+        self.screen.blit(level_text, level_rect)
+
+    def update_level(self):
+        # Update level every 200 points and increase letters falling speed.
+        if self.score % 200 == 0 and self.score != 0:
+            self.level += 1
+            self.settings.falling_speed += 1
 
 
 class Lives:
+    """ Class responsible for player lives. """
     def __init__(self, settings, screen):
         self.lives = 3
         self.settings = settings
         self.screen = screen
 
     def draw(self):
+        # Draw lives on screen.
         life_font = self.settings.game_font
         life_text = life_font.render(f"Lives: {self.lives}", True, (255, 255, 255))
         life_rect = life_text.get_rect(topleft=(100, 10))
         self.screen.blit(life_text, life_rect)
 
     def decrease(self):
+        # Decrease live when letter touch bottom of the screen.
         self.lives -= 1
 
 
 class FallingLetter(pygame.sprite.Sprite):
-    """ Function responsible for falling letters. """
+    """ Class responsible for falling letters. """
     def __init__(self, letter, settings, alphabet):
         super().__init__()
         self.alphabet = alphabet
@@ -50,15 +67,17 @@ class FallingLetter(pygame.sprite.Sprite):
         self.rect.y += self.settings.falling_speed
 
     def hit(self, key, score):
-
         if key.lower() == self.alphabet.get_letter(self.image).lower():
-            score.score += self.settings.points  # Add 10 points to score
+            # Add points to score when hit
+            score.score += self.settings.points
+            score.update_level()
             return True
         else:
             return False
 
 
 class Game:
+    """ Class responsible for game logic. """
     def __init__(self):
         pygame.init()
         self.settings = GameSettings()
@@ -98,6 +117,7 @@ class Game:
                 exit()
 
     def run_game(self):
+        # Main loop of the game.
         self.start_game()  # Wait for space key to start game
         # Start main loop of the game.
         while True:
@@ -130,7 +150,7 @@ class Game:
             self.screen.blit(self.background, (0, 0))
             self.falling_letters.update()
             self.falling_letters.draw(self.screen)
-            self.score.update()
+            self.score.draw()
             self.lives.draw()
             pygame.display.update()
 
@@ -138,14 +158,22 @@ class Game:
             self.clock.tick(self.settings.max_frame)
 
     def game_over(self):
+        # Game over screen.
         font = self.settings.start_font
         game_over = font.render("GAME OVER", True, (255, 0, 0))
         game_over_rect = game_over.get_rect(center=self.screen.get_rect().center)
         self.screen.blit(game_over, game_over_rect)
         pygame.display.update()
-        pygame.time.wait(2000)  # Pause for 2 seconds.
-        pygame.display.update()
-        self.run_game()
+
+        # Wait for space key to be pressed for restart game.
+        while True:
+            event = pygame.event.wait()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.__init__()  # Restart the game
+                self.run_game()
+            elif event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
 
 if __name__ == '__main__':
